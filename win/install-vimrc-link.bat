2f10cb5a0fcdfeef515f468f9cde7d106a82279a
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-05-08 15:04:43 -0300

    Windows batch script for easy double-click installation DONE.

--- win/install-vimrc-link.bat
+++ win/install-vimrc-link.bat
@@ -1,5 +1,8 @@
-REM copying the main vimrc config file to your HOME directory
-REM and adjusting the paths so that VIM can bootstrap itself correctly.
+@echo off
+
+
+echo Copying the main vimrc config file to your HOME directory
+echo and adjusting the paths so that VIM can bootstrap itself correctly.
 
 set WINVIM_DIR=%CD%
 cd ..
@@ -7,12 +10,15 @@ cd ..
 set PARENT_DIR=%CD%
 cd %WINVIM_DIR%
 
-@echo off
-echo ^
-let $VIMROOT_PARENT = "%PARENT_DIR%" ^
+set TARGET="C:%HOMEPATH%\_vimrc"
 
 
-let $VIMROOT = expand("$VIMROOT_PARENT") . "\\modular-xplatform-vim-cfg" ^
 
-source $VIMROOT\config\vimrc.vim ^
- > "%HOMEPATH%\_vimrc"
+echo let $VIMROOT_PARENT = '%PARENT_DIR%' >> _vimrc
+echo let $VIMROOT = expand("$VIMROOT_PARENT") . '\modular-xplatform-vim-cfg' >> _vimrc
+echo source $VIMROOT\config\vimrc.vim >> _vimrc
+
+move _vimrc %TARGET%
+
+
+echo Job DONE. Now you can just launch VIM normally :)
