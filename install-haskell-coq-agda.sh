50be777362ca9c89d58869fc6afcf3e1933cf927
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-12-28 12:50:54 +0100

    removed foreach subm pull from install scripts, subm update already does that

--- install-haskell-coq-agda.sh
+++ install-haskell-coq-agda.sh
@@ -6,7 +6,6 @@ DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 cd "${DIR}"
 git checkout extra-haskell-coq-agda
 git submodule update --init --recursive --remote
-git submodule foreach --recursive 'git pull'
 cd -
 
 ln -s -f "${DIR}/_vimrc"  "${HOME}/_vimrc"
