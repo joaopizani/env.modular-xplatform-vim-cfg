f229ba5946f4b1794648522afc13d1188edad681
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-12-28 12:36:33 +0100

    updated install script with submodule recursive foreach git pull and update subm remote

--- install-basic.sh
+++ install-basic.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
-FROMDIR="${PWD}"
 
 
 cd "${DIR}"
 git submodule update --init --recursive --remote
-git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch'
-cd "${FROMDIR}"
+git submodule foreach --recursive 'git pull'
+cd -
 
 ln -s -f "${DIR}/_vimrc"  "${HOME}/_vimrc"
 
