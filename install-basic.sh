5fba5b649662f9e22e69f465e223b4cc2d672951
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-12-28 12:50:54 +0100

    removed foreach subm pull from install scripts, subm update already does that

--- install-basic.sh
+++ install-basic.sh
@@ -5,7 +5,6 @@ DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 
 cd "${DIR}"
 git submodule update --init --recursive --remote
-git submodule foreach --recursive 'git pull'
 cd -
 
 ln -s -f "${DIR}/_vimrc"  "${HOME}/_vimrc"
