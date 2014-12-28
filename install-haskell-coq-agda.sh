a0c6e30f6431b59ccb4ac5c9fc5c6c3096af05aa
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-12-28 12:36:33 +0100

    updated install script with submodule recursive foreach git pull and update subm remote

--- install-haskell-coq-agda.sh
+++ install-haskell-coq-agda.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
-FROMDIR="${PWD}"
 
 
 cd "${DIR}"
 git checkout extra-haskell-coq-agda
 git submodule update --init --recursive --remote
-git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch'
-cd "${FROMDIR}"
+git submodule foreach --recursive 'git pull'
+cd -
 
 ln -s -f "${DIR}/_vimrc"  "${HOME}/_vimrc"
 
 vim +NeoBundleInstall +qall
+
